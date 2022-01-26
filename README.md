eb17f55d0c038cd7248f0fa3632a1f26c5da003e
ben21 <bvanbever1@gmail.com>
2022-01-26 11:59:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-4la1vs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-4la1vs)
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+import "./style.css";
+
+export default function App() {
+
+  const [films, setFilms] = useState([])
+
+  useEffect(() => {
+    
+  },[])
+
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
